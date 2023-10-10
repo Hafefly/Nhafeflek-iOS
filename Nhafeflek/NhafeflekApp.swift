9f300e450d073ab40ea6e558ca8ddf4e7f20ec18
RMehdid <s.mahdid@univ-boumerdes.dz>
2023-10-10 19:49:57 +0100

    initialization of project

---
+++ Nhafeflek/NhafeflekApp.swift
@@ -0,0 +1,17 @@
+//
+//  NhafeflekApp.swift
+//  Nhafeflek
+//
+//  Created by Samy Mehdid on 10/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct NhafeflekApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
