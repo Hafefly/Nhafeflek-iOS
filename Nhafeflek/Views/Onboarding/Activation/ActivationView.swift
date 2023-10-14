62943202af1c20eb82c8fd7c4a65df1811a7fda1
RMehdid <s.mahdid@univ-boumerdes.dz>
2023-10-14 21:33:40 +0100

    added addmission and activation views

--- Nhafeflek/Views/Onboarding/Activation/ActivationView.swift
+++ Nhafeflek/Views/Onboarding/Activation/ActivationView.swift
@@ -9,6 +9,38 @@ import SwiftUI
 
 struct ActivationView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ViewLayout {
+            HeaderView(title: "activate account")
+        } content: { edges in
+            VStack{
+                Text("please head to our headquartered  to activate your account")
+                    .font(.primaryColor, .semiBold, 12)
+                    .frame(width: .infinity)
+                    .background(Color.white)
+                VStack(alignment: .leading){
+                    Text("account informations")
+                        .font(.primaryColor, .semiBold, 20)
+                    VStack{
+                        Text("fullname")
+                            .font(.primaryColor, .medium, 16)
+                        Text("Samy Abderraouf Mehdid")
+                            .font(.white, .semiBold, 16)
+                            .padding(16)
+                            .frame(width: .infinity)
+                            .cornerRadius(6)
+                    }
+                }
+                Spacer()
+            }
+            .padding(16)
+            .background(
+                ZStack{
+                    Image("BackgroundImage")
+                        .blur(radius: 6)
+                    LinearGradient.hfGradient
+                }
+                .ImagePattern()
+            )
+        }
     }
 }
--- Nhafeflek/Views/Onboarding/Admission/AdmissionView.swift
+++ Nhafeflek/Views/Onboarding/Admission/AdmissionView.swift
@@ -9,6 +9,32 @@ import SwiftUI
 
 struct AdmissionView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ViewLayout {
+            HeaderView(title: "GET A BARBERSHOP")
+        } content: { edges in
+            VStack{
+                Spacer()
+                Image("qr_code")
+                    .resizable()
+                    .frame(width: 284, height: 284)
+                Spacer()
+                Spacer()
+                Text("show this to your barbershop, as soon as he scans it your are added automatically as one of his barbers™")
+                    .font(.primaryColor, .medium, 20)
+                    .padding()
+                    .background(Color.white)
+                    .cornerRadius(12)
+                Spacer()
+            }
+            .padding(16)
+            .background(
+                ZStack{
+                    Image("BackgroundImage")
+                        .blur(radius: 6)
+                    LinearGradient.hfGradient
+                }
+                .ImagePattern()
+            )
+        }
     }
 }
